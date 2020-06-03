eb4bca3da661f759c2a3cbca4dc384150ff263a3
brubub <63169178+brubub@users.noreply.github.com>
2020-06-03 14:10:13 +0200

    Add files via upload

---
+++ src/com/github/brubub/stdefgen/exceptions/ImpossibleToContinueExecutionException.java
@@ -0,0 +1,15 @@
+package com.github.brubub.stdefgen.exceptions;
+
+public class ImpossibleToContinueExecutionException extends Exception {
+
+	private static final long serialVersionUID = -8236353525426104590L;
+
+	public ImpossibleToContinueExecutionException(String arg0) {
+		super(arg0);
+	}
+
+	public ImpossibleToContinueExecutionException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+}
