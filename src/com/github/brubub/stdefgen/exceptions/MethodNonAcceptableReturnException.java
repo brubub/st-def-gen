418571dab2bafc618b8c736f29d57d40a3dca7ca
brubub <63169178+brubub@users.noreply.github.com>
2020-06-03 14:09:47 +0200

    Create MethodNonAcceptableReturnException.java

---
+++ src/com/github/brubub/stdefgen/exceptions/MethodNonAcceptableReturnException.java
@@ -0,0 +1,15 @@
+package com.github.brubub.stdefgen.exceptions;
+
+public class MethodNonAcceptableReturnException extends Exception {
+
+	private static final long serialVersionUID = -2691519519760982471L;
+
+	public MethodNonAcceptableReturnException(String arg0) {
+		super(arg0);
+	}
+
+	public MethodNonAcceptableReturnException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+}
